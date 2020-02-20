2d5d7dfa073989eecd2c479893e8ee41f1ececa2
ince4 <421603832@qq.com>
2020-02-20 14:02:37 +0800

    view/BookList

--- src/views/BookList.vue
+++ src/views/BookList.vue
@@ -8,9 +8,19 @@
         <span class="iconfont icon-fanhui" @click="$router.go(-1)"/>
       </template>
     </m-header>
+    <!-- tags -->
+    <ul class="tab">
+      <li
+      class="tags"
+      v-for="item of typeList"
+      :key="item.id"
+      :class="{'active': tagActive === item.order, '': tagActive !== item.order}"
+      @click="tagSelected(item.order)">
+        {{item.name}}
+      </li>
+    </ul>
     <!-- 书籍列表 -->
-    <book-list :booksData="booksData" :listLength="15">
-    </book-list>
+    <book-list :booksData="booksData" :listLength="15"/>
     </div>
   </div>
 </template>
@@ -27,33 +37,38 @@ export default {
       booksData: [],
       typeList: [
         {
+          order: 0,
           id: 'hot',
           name: '热门'
         },
         {
+          order: 1,
           id: 'new',
           name: '新书'
         },
         {
+          order: 2,
           id: 'reputation',
           name: '好评'
         },
         {
+          order: 3,
           id: 'over',
           name: '完结'
         },
         {
+          order: 4,
           id: 'monthly',
           name: '包月'
         }
-      ]
+      ],
+      tagActive: 0
     }
   },
   created () {
     api.getBooks(this.$route.query.gender, this.typeList[0].id, this.$route.query.major, '', 0, 15)
       .then(res => {
         this.booksData = res.data.books
-        console.log(this.bookData)
         this.isLoaded = true
       })
   },
@@ -63,11 +78,41 @@ export default {
     BookList
   },
   methods: {
+    tagSelected: function (order) {
+      this.tagActive = order
+      this.isLoaded = false
+      api.getBooks(this.$route.query.gender, this.typeList[this.tagActive].id, this.$route.query.major, '', 0, 15)
+      .then(res => {
+        this.booksData = res.data.books
+        this.isLoaded = true
+      })
+    }
   }
 }
 </script>
 <style lang="scss" scoped>
 .booklist {
   @include wrap-scroll;
+  height: calc(100vh - 85px);
+  margin-top: calc(30px);
+  .tab {
+    width: 100vw;
+    position: fixed;
+    background: rgb(255, 255, 255);
+    height: 40px;
+    top: 6.8vh;
+    white-space: nowrap;
+    .tags {
+      display: inline-block;
+      font-size: 13px;
+      padding: 0 10px;
+      height: 100%;
+      line-height: 40px;
+      color: #666;
+      &.active {
+        color: #f94040;
+      }
+    }
+  }
 }
 </style>
